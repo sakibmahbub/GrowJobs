e4d110e94f90d561f47d205b0e388be68a12aa9c
sakibmahbub <arafatsakib48@gmail.com>
2023-04-10 03:10:42 +0600

    created footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className="footer p-10 bg-gray-900 text-white ">
+      <div>
+        <h3 className="text-2xl font-bold">Grow Jobs</h3>
+        <p className="text-gray-300">
+          Grow Jobs Ltd. Providing reliable job since 2110
+        </p>
+        <div className="w-20">
+          <img className="w-full" src="../assets/Icons/Group9969.png" alt="" />
+        </div>
+      </div>
+      <div>
+        <span className="text-white font-semibold text-lg">Company</span>
+        <a className="text-gray-300">About Us</a>
+        <a className="text-gray-300">Work</a>
+        <a className="text-gray-300">Latest News</a>
+        <a className="text-gray-300">Careers</a>
+      </div>
+      <div>
+        <span className="text-white font-semibold text-lg">Product</span>
+        <a className="text-gray-300">Prototype</a>
+        <a className="text-gray-300">Plans & Pricing</a>
+        <a className="text-gray-300">Customers</a>
+        <a className="text-gray-300">Integrations</a>
+      </div>
+      <div>
+        <span className="text-white font-semibold text-lg">Support</span>
+        <a className="text-gray-300">Help Desk</a>
+        <a className="text-gray-300">Sales</a>
+        <a className="text-gray-300">Become A Partner</a>
+        <a className="text-gray-300">Developers</a>
+      </div>
+      <div>
+        <span className="text-white font-semibold text-lg">Contact</span>
+        <a className="text-gray-300">524 Broadway , NYC</a>
+        <a className="text-gray-300">+1 777 - 978 - 5570</a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
